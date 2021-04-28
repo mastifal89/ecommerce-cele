c8ddb09672651804100806726158776c078bacf9
mastifal89 <juanrata89@gmail.com>
2021-04-28 13:47:25 -0300

    first commit

---
+++ components/Header/Menu/Menu.js
@@ -0,0 +1,9 @@
+import { Container, Menu, Grid, Icon, Label } from "semantic-ui-react";
+
+export default function MenuWeb() {
+  return (
+    <div>
+      <h2>Menu...</h2>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import BasicLayout from "../layouts/BasicLayout";
+export default function Home() {
+  return (
+    <BasicLayout>
+      <h1>Estamos en Next JS</h1>
+    </BasicLayout>
+  );
+}
